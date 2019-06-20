40ff79b10a4f7697b02a0a9ae46956ccff5fe65b
VitaliyTregubovKh <tregubovvakharkov@gmail.com>
2019-06-20 15:53:45 +0300

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,41 @@
+import { FLIGHT_TYPES } from './constants';
+
+export function CreateFlightMapper(type) {
+  return (flightItem) => {
+    const base = {
+      id: flightItem.ID,
+      terminal: flightItem.term,
+      airline: flightItem.airline
+        && flightItem.airline.en
+        && flightItem.airline.en.name,
+      flightNumber: flightItem.codeShareData
+        && flightItem.codeShareData[0]
+        && flightItem.codeShareData[0].codeShare,
+      status: flightItem.status,
+    };
+
+    switch (type) {
+      case FLIGHT_TYPES.DEPARTURE:
+        return {
+          ...base,
+          gate: flightItem.gateNo,
+          time: flightItem.timeDepShedule,
+          destination: flightItem['airportToID.name_en']
+        };
+      case FLIGHT_TYPES.ARRIVAL:
+        return {
+          ...base,
+          time: flightItem.timeArrShedule,
+          destination: flightItem['airportFromID.name_en']
+        }
+    }
+  };
+}
+
+export function CreateDateString(date) {
+  const monthDay = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${monthDay}-${month}-${year}`;
+}
