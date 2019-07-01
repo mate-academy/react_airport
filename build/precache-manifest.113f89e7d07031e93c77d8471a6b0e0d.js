c3674e545b90fa84cdca7a51531f689180d7bd56
Stas Tselovanskyi <solmirx1@gmail.com>
2019-07-01 12:17:51 +0300

    build_the_progect

---
+++ build/precache-manifest.113f89e7d07031e93c77d8471a6b0e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe2dd2a4955f8a6f2c3c89b2bf5d28c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ac58150fe8360553997",
+    "url": "./static/css/main.ee403d12.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "./static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "8ac58150fe8360553997",
+    "url": "./static/js/main.dea48e5e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
