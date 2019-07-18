b92a661a07ca3f05ff45e235bd4a7d339c6f47ae
AlexHladun <gefest.gl@gmail.com>
2019-07-18 10:49:53 +0300

    Solution master

---
+++ build/precache-manifest.8451e6ea705b3187de886a0aafc2cbc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f0a54b9c5b728f8bcea677f37ddcbe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7709b8fbc7c2d173e741",
+    "url": "./static/css/main.b4cb6267.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "./static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "7709b8fbc7c2d173e741",
+    "url": "./static/js/main.2a7b419a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
